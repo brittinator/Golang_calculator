7abb5d4f233687d3d4a746bb1cb426a46aad419b
Brittany Walentin <brittw@uber.com>
2016-06-25 23:45:36 -0700

    First commit, WIP calculator.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Calculator
+
+Build a calculator command line interface (CLI) that allows a user to perform simple arithmetic. The program should ask for an operation (addition, subtraction, multiplication, division) and two numbers, then give the result of applying the operation to the two numbers.
+
+The program should accept both the name (add) and the symbol (+) for each possible operation.
---
+++ calculator.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	// "math"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello there! So you want to use this calculator?")
+	operator()
+}
+
+func operator() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What operation would you like to perform?")
+	operation, _ := reader.ReadString('\n')
+	op := strings.TrimSpace(operation)
+	if op == "+" || op == "add" {
+		fmt.Println(op)
+		nums()
+	} else if op == "-" || op == "sub" {
+
+	} else if op == "*" || op == "mul" {
+	} else if op == "/" || op == "div" {
+	} else {
+		fmt.Println("I'm not sure what you mean. Exiting")
+	}
+}
+
+func nums() (int, int) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter your first number: ")
+	firStr, _ := reader.ReadString('\n')
+	firInt := strings.TrimSpace(firStr)
+	firInt, _ := strconv.Atoi(firInt)
+	fmt.Println("Please enter your second number: ")
+	sec, _ := reader.ReadString('\n')
+	return firInt, sec
+}
